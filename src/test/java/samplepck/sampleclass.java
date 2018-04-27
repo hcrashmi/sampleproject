73866d5e4a1de32d8fa4b417c131be59ff47c412
Shekhar741 <34434026+Shekhar741@users.noreply.github.com>
2018-04-27 17:59:39 +0530

    aaaaa

--- src/test/java/samplepck/sampleclass.java
+++ src/test/java/samplepck/sampleclass.java
@@ -3,9 +3,10 @@ package samplepck;
 public class sampleclass {
 
 	public static void main(String[] args) {
-		
+		System.out.println("hi hi");
 		System.out.println("hi");
 		System.out.println("bye");
+		System.out.println("bye bye");
 
 	}
 
