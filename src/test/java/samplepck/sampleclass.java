b17ac7d907549510d4ea71e31e74ae044844be19
hcrashmi <37067788+hcrashmi@users.noreply.github.com>
2018-04-27 17:54:21 +0530

    Update sampleclass.java

--- src/test/java/samplepck/sampleclass.java
+++ src/test/java/samplepck/sampleclass.java
@@ -3,7 +3,7 @@ package samplepck;
 public class sampleclass {
 
 	public static void main(String[] args) {
-		
+		System.out.println("hi hi");
 		System.out.println("hi");
 		System.out.println("bye");
 
